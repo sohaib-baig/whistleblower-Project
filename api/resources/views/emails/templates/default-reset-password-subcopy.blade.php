98f70da9596e9cb586fe613b5e47d35daf771e51
devops <xyz>
2026-02-18 21:04:44 +0500

    final commit

--- api/resources/views/emails/templates/default-reset-password-subcopy.blade.php
+++
@@ -1,3 +0,0 @@
-If you're having trouble clicking the "Reset password" button, copy and paste this URL into your web browser:<br>
-<a href="{{ $resetUrl }}" style="color:#1e293b;word-break:break-all;text-decoration:none;">{{ $resetUrl }}</a>
-
--- api/resources/views/emails/templates/default-welcome-set-password-subcopy.blade.php
+++
@@ -1,3 +0,0 @@
-If you're having trouble clicking the "Set password" button, copy and paste this URL into your web browser:<br>
-<a href="{{ $setPasswordUrl }}" style="color:#1e293b;word-break:break-all;text-decoration:none;">{{ $setPasswordUrl }}</a>
-
